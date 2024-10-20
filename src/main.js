0ba4efc5603bcb8b800c66ff5f62697b0cf72a8b
Tomasz Ostaszewski <ost.tomasz@gmail.com>
2024-10-20 16:03:52 +0200

    little adjustments

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import './js/hero.js';
 import './js/hero_modals.js';
 import './js/home.js';
-import './js/library.js';
 import './js/footer.js';
+import './js/catalog.js';
+import './js/library.js';
