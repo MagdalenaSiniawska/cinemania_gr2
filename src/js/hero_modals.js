c7d4a15357d5d8f0f430190498e1068e1eb8acac
achoma <139071702+achoma@users.noreply.github.com>
2024-10-22 04:35:31 +0200

    style-modal-hero

--- src/js/hero_modals.js
+++ src/js/hero_modals.js
@@ -1,24 +1,25 @@
-// Modal dla zwiastuna filmu 
-export const openTrailerModal = (trailerData) => {
+// Modal dla zwiastuna filmu
+export const openTrailerModal = trailerData => {
   const modalTrailer = document.getElementById('modal-trailer');
   const trailer = trailerData.results.find(vid => vid.type === 'Trailer');
 
   if (trailer) {
     modalTrailer.innerHTML = `
-      <div class="modal-content">
-        <span class="close-modal">&times;</span>
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/${trailer.key}" 
-          frameborder="0" allowfullscreen></iframe>
-      </div>
-    `;
+          <div class="modal-content">
+            <span class="close-modal">&times;</span>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/${trailer.key}"
+              frameborder="0" allowfullscreen></iframe>
+          </div>
+        `;
   } else {
     modalTrailer.innerHTML = `
-      <div class="modal-content error-pop-up">
-        <span class="close-modal">&times;</span>
-        <img src="../images/oops-logo.png" alt="camera icon with popcorn" width="363" height="318"/>
-        <p>OOPS... We are very sorry! But we couldn’t find the trailer.</p>
-      </div>
-    `;
+          <div class="modal-content error-pop-up">
+            <span class="close-modal">&times;</span>
+              <p>OOPS... </br>We are very sorry! </br>But we couldn’t find the trailer.</p>
+               <img src="../images/oops-logo.png" alt="camera icon with popcorn" width="363" height="318"/>
+
+          </div>
+        `;
   }
 
   modalTrailer.style.display = 'block';
@@ -29,7 +30,7 @@ export const openTrailerModal = (trailerData) => {
   });
 
   // Zamknięcie modala przez kliknięcie w tło modala
-  modalTrailer.addEventListener('click', (event) => {
+  modalTrailer.addEventListener('click', event => {
     if (event.target === modalTrailer) {
       modalTrailer.style.display = 'none';
     }
@@ -37,7 +38,8 @@ export const openTrailerModal = (trailerData) => {
 };
 
 // Modal dla szczegółów filmu
-export const openDetailsModal = (details) => {
+
+export const openDetailsModal = details => {
   const modalDetails = document.getElementById('modal-details');
 
   const backgroundImage = details.backdrop_path
@@ -45,16 +47,31 @@ export const openDetailsModal = (details) => {
     : `https://image.tmdb.org/t/p/original/${details.poster_path}`;
 
   modalDetails.innerHTML = `
-    <div class="modal-content" style="background-image: url('${backgroundImage}'); background-size: cover; background-position: center; padding: 20px; color: white;">
-      <span class="close-modal" style="cursor: pointer; font-size: 24px;">&times;</span>
-      <h2>${details.title}</h2>
-      <p><strong>Vote / Votes:</strong> ${details.vote_average} / ${details.vote_count}</p>
-      <p><strong>Popularity:</strong> ${details.popularity}</p>
-      <p><strong>Genres:</strong> ${details.genres.map(genre => genre.name).join(', ')}</p>
-      <p><strong>Overview:</strong> ${details.overview}</p>
-      <button id="add-to-library" class="add-to-library-btn">Add to my library</button>
+    <div class="modal-content">
+      <span class="close-modal">&times;</span> <!-- Przeniesiono przycisk zamykania tutaj -->
+      <div class="modal-image" style="background-image: url('${backgroundImage}');"></div>
+      <div class="modal-text">
+        <h2>${details.title}</h2>
+        <div class="tags-grade-wrap">
+        <div>
+        <p><strong>Vote / Votes:</strong></p>
+        <p><strong>Popularity:</strong></p>
+        <p><strong>Genres:</strong></p>
+</div><div>
+
+<p> ${details.vote_average} / ${details.vote_count}</p>        
+<p> ${details.popularity}</p>          
+<p> ${details.genres.map(genre => genre.name).join(', ')}</p>
+    </div></div>
+    <div class="modal-text-about">
+        <p ><strong>ABOUT:</strong></p>
+        <p class="modal-text-about">${details.overview}</p>
+    </div>
+        <button id="add-to-library" class="add-to-library-btn">Add to my library</button>
+      </div>
     </div>
   `;
+
   modalDetails.style.display = 'block';
 
   // Zamknięcie modala dla szczegółów filmu przez ikonę zamknięcia
@@ -63,7 +80,7 @@ export const openDetailsModal = (details) => {
   });
 
   // Zamknięcie modala przez kliknięcie w tło modala
-  modalDetails.addEventListener('click', (event) => {
+  modalDetails.addEventListener('click', event => {
     if (event.target === modalDetails) {
       modalDetails.style.display = 'none';
     }
